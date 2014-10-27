737b71966a823782166b078f3013f3f658a61443
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-10-26 22:32:25 -0400

    Listar os veiculos cadastrados.

---
+++ Veiculos/Controllers/VeiculoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Veiculos.DAO;
+using Veiculos.Models;
+
+namespace Veiculos.Controllers
+{
+    public class VeiculoController : Controller
+    {
+
+        private readonly VeiculoDAO _veiculoDao;
+
+        public VeiculoController(VeiculoDAO veiculoDao)
+        {
+            _veiculoDao = veiculoDao;
+        }
+
+        public ActionResult Index()
+        {
+            IList<Veiculo> veiculos = _veiculoDao.GetAll();
+
+            return View(veiculos);
+        }
+    }
+}
