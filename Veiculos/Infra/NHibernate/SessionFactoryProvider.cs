7bcb794495ff842f48385262d99e9f24df9ef20a
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-10-29 01:31:51 -0400

    Refatorando SessionFactoryProvider.

--- Veiculos/Infra/NHibernate/SessionFactoryProvider.cs
+++ Veiculos/Infra/NHibernate/SessionFactoryProvider.cs
@@ -1,5 +1,8 @@
-﻿using NHibernate;
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
 using Ninject.Activation;
+using Veiculos.Models.Mapping;
 
 namespace Veiculos.Infra.NHibernate
 {
@@ -7,9 +10,21 @@ namespace Veiculos.Infra.NHibernate
     {
         protected override ISessionFactory CreateInstance(IContext context)
         {
-            var sessionFactory = new SessionFactoryCreator();
-
-            return sessionFactory.CreateSessionFactory();
+            return BuildSessionFactory();
         }
+
+        public ISessionFactory BuildSessionFactory()
+        {
+            return Fluently.Configure()
+                .Database(
+                    MsSqlConfiguration.MsSql2008.ConnectionString(
+                        @"Data Source=G1711MAX\sqlexpress;Password=chapado;User ID=sa;Initial Catalog=veiculos;Application Name=Veiculos;")
+                        .ShowSql())
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<VeiculoMap>())
+                .ExposeConfiguration(c => c.SetProperty("generate_statistics", "true"))
+                //.ExposeConfiguration(c => new SchemaExport(c).Create(false, true)) // TODO Remover depois de definir todos os models
+                .BuildSessionFactory();
+        }
+
     }
 }
