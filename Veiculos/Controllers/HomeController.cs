b87d2b9d0399e8f5c073d7a408412b066af2561f
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-11-01 02:52:38 -0400

    Excluindo action Contact do controller Home. Exemplo angularjs recebendo dados do controller.

--- Veiculos/Controllers/HomeController.cs
+++ Veiculos/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Antlr.Runtime.Misc;
 using Veiculos.DAO;
+using Veiculos.Models;
 
 namespace Veiculos.Controllers
 {
@@ -25,11 +29,16 @@ namespace Veiculos.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Todo()
         {
-            ViewBag.Message = "Your contact page.";
+            IList<Todo> todos = new List<Todo>();
+            todos.Add(new Todo { Action = "Buy Flowers", Done = false });
+            todos.Add(new Todo { Action = "Get Shoes", Done = false });
+            todos.Add(new Todo { Action = "Collect Tickets", Done = true });
+            todos.Add(new Todo { Action = "Call Joe", Done = false });
 
-            return View();
+            return Json(todos, JsonRequestBehavior.AllowGet);
         }
+
     }
 }
---
+++ Veiculos/Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Veiculos.Models
+{
+    public class Todo
+    {
+        public String Action { get; set; }
+        public bool Done { get; set; }
+    }
+}
