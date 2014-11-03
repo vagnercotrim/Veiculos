c95d4168876133e8a1c99aacb4570decae1f3faf
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-11-03 14:54:59 -0400

    Verifica se já existe um funcionário com o cadastro informado no formulário.

--- Veiculos/Models/Validation/FuncionarioValidation.cs
+++ Veiculos/Models/Validation/FuncionarioValidation.cs
@@ -1,16 +1,32 @@
 ﻿using FluentValidation;
+using Veiculos.DAO;
 
 namespace Veiculos.Models.Validation
 {
     public class FuncionarioValidation : AbstractValidator<Funcionario>
     {
 
-        public FuncionarioValidation()
+        private readonly FuncionarioDAO _dao;
+
+        public FuncionarioValidation(FuncionarioDAO dao)
         {
+            _dao = dao;
+            
             RuleFor(f => f.Cadastro).NotEmpty();
             RuleFor(f => f.Nome).NotEmpty();
             RuleFor(f => f.Cargo).NotEmpty();
+
+            RuleFor(f => f.Cadastro).Must((funcionario, cadastro) => CadastroDisponivel(funcionario)).WithMessage("Este cadastro já está em uso.");
         }
 
+        private bool CadastroDisponivel(Funcionario funcionario)
+        {
+            Funcionario noBanco = _dao.FindByCadastro(funcionario.Cadastro);
+
+            if (noBanco == null)
+                return true;
+
+            return noBanco.Id == funcionario.Id;
+        }
     }
 }
