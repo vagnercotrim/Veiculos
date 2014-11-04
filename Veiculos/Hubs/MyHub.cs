73be158b015fc6dd4f1475f6dbced57b0945b7b4
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-11-03 21:25:15 -0400

    Instalando SignalR e implementando um exemplo simples que atualiza as horas no cliente a cada segundo.

---
+++ Veiculos/Hubs/MyHub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Veiculos.Hubs
+{
+    public class MyHub : Hub
+    {
+
+        public MyHub()
+        {
+            // Create a Long running task to do an infinite loop which will keep sending the server time
+            // to the clients every 3 seconds.
+            var taskTimer = Task.Factory.StartNew(async () =>
+            {
+                while (true)
+                {
+                    string timeNow = DateTime.Now.ToString();
+                    //Sending the server time to all the connected clients on the client method SendServerTime()
+                    Clients.All.SendServerTime(timeNow);
+                    //Delaying by 3 seconds.
+                    await Task.Delay(1000);
+                }
+            }, TaskCreationOptions.LongRunning
+                );
+        }
+
+    }
+}
---
+++ Veiculos/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Veiculos.Startup))]
+namespace Veiculos
+{
+    public class Startup
+    {
+        
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+
+    }
+}
