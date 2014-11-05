b01cf4039a4f4dfc6bfb1bccb26fe9a899157e34
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-11-04 21:49:42 -0400

    Refatoran as classes de Notice.

--- Veiculos/Infra/Notice/NoticeHelper.cs
+++ Veiculos/Infra/Notice/NoticeHelper.cs
@@ -7,15 +7,6 @@ namespace System.Web.Mvc
 {
     public static class NoticeHelper
     {
-        public static IDictionary<String, NoticeType> GetNoticesFromTempData(this HtmlHelper htmlHelper)
-        {
-            var notices = htmlHelper.ViewContext.Controller.TempData["notices"];
-
-            if (notices == null)
-                return new Dictionary<String, NoticeType>();
-
-            return (Dictionary<String, NoticeType>)notices;
-        }
 
         public static IDictionary<String, NoticeType> GetNoticesFromTempData(this ControllerBase controller)
         {
@@ -66,25 +57,6 @@ namespace System.Web.Mvc
         public static void Error(this ControllerBase controller, string mensagem)
         {
             AddNotice(controller, mensagem, NoticeType.Error);
-        }
-
-        private static MvcHtmlString Toastr(this HtmlHelper htmlHelper, IDictionary<String, NoticeType> erros)
-        {
-            if (erros.IsNotEmpty())
-            {
-                String html = @"<script type=""text/javascript"">$(function () {toastr.options = {""closeButton"": true,""positionClass"": ""toast-bottom-right"",""onclick"": null,""showDuration"": ""0"",""hideDuration"": ""0"",""timeOut"": ""0"",""showMethod"": ""fadeIn""};#notices#});</script>";
-                String notices = "";
-                foreach (KeyValuePair<string, NoticeType> par in erros)
-                    notices += String.Format("toastr.{0}('{1}');", par.Value.ToLowerInvariantString(), par.Key);
-                return MvcHtmlString.Create(html.Replace("#notices#", notices));
-            }
-
-            return MvcHtmlString.Empty;
-        }
-
-        public static MvcHtmlString Toastr(this HtmlHelper htmlHelper)
-        {
-            return Toastr(htmlHelper, GetNoticesFromTempData(htmlHelper));
         }
 
     }
---
+++ Veiculos/Infra/Notice/ToastrHelper.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using FluentNHibernate.Conventions;
+using FluentNHibernate.Utils;
+using Veiculos.Infra.Notice;
+
+namespace System.Web.Mvc
+{
+    public static class ToastrHelper
+    {
+
+        public static IDictionary<String, NoticeType> GetNoticesFromTempData(this HtmlHelper htmlHelper)
+        {
+            var notices = htmlHelper.ViewContext.Controller.TempData["notices"];
+
+            if (notices == null)
+                return new Dictionary<String, NoticeType>();
+
+            return (Dictionary<String, NoticeType>)notices;
+        }
+
+        private static MvcHtmlString Toastr(this HtmlHelper htmlHelper, IDictionary<String, NoticeType> erros)
+        {
+            if (erros.IsNotEmpty())
+            {
+                var html = BuildScript(erros);
+
+                return MvcHtmlString.Create(html);
+            }
+
+            return MvcHtmlString.Empty;
+        }
+
+        private static string BuildScript(IDictionary<string, NoticeType> erros)
+        {
+            String toastrOptions = @"toastr.options = {""closeButton"": true,""positionClass"": ""toast-bottom-right"",""onclick"": null,""showDuration"": ""0"",""hideDuration"": ""0"",""timeOut"": ""0"",""showMethod"": ""fadeIn""}";
+            String html = @"<script type=""text/javascript"">$(function () {" + toastrOptions + ";#notices#});</script>";
+            
+            String notices = "";
+
+            foreach (KeyValuePair<string, NoticeType> par in erros)
+                notices += String.Format("toastr.{0}('{1}');", par.Value.ToLowerInvariantString(), par.Key);
+            
+            return html.Replace("#notices#", notices);
+        }
+
+        public static MvcHtmlString Toastr(this HtmlHelper htmlHelper)
+        {
+            return Toastr(htmlHelper, GetNoticesFromTempData(htmlHelper));
+        }
+
+    }
+}
