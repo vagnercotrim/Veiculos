a2436f1bc9e320c2c1184092c6be5be3dbfe0f48
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-11-07 23:04:07 -0400

    Implementando cadastro de nova autorização para circulação de veículo (obs.: ainda falta algumas validações)

--- Veiculos/Controllers/AutorizacaoController.cs
+++ Veiculos/Controllers/AutorizacaoController.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using FluentValidation.Results;
 using Veiculos.DAO;
 using Veiculos.Infra.NHibernate;
 using Veiculos.Models;
+using Veiculos.Models.Validation;
 
 namespace Veiculos.Controllers
 {
@@ -13,19 +12,76 @@ namespace Veiculos.Controllers
     {
 
         private readonly AutorizacaoCirculacaoDAO _autorizacaoCirculacaoDao;
+        private readonly VeiculoDAO _veiculoDao;
+        private readonly MotoristaDAO _motoristaDao;
+        private readonly FuncionarioDAO _funcionarioDao;
+        private readonly AutorizacaoCirculacaoValidation _validation;
 
-        public AutorizacaoController(AutorizacaoCirculacaoDAO autorizacaoCirculacaoDao)
+        public AutorizacaoController(AutorizacaoCirculacaoDAO autorizacaoCirculacaoDao, VeiculoDAO veiculoDao,
+            MotoristaDAO motoristaDao, FuncionarioDAO funcionarioDao, AutorizacaoCirculacaoValidation validation)
         {
             _autorizacaoCirculacaoDao = autorizacaoCirculacaoDao;
+            _veiculoDao = veiculoDao;
+            _motoristaDao = motoristaDao;
+            _funcionarioDao = funcionarioDao;
+            _validation = validation;
         }
 
 
         [Route("autorizacao")]
         public ActionResult Index(int pagina = 1)
         {
-            Paging<AutorizacaoCirculacao> veiculos = _autorizacaoCirculacaoDao.GetAll(pagina, 5);
+            Paging<AutorizacaoCirculacao> veiculos = _autorizacaoCirculacaoDao.GetAll(pagina, 15);
 
             return View(veiculos);
+        }
+
+        [Route("autorizacao/novo")]
+        public ActionResult Novo(int id)
+        {
+            Veiculo veiculo = _veiculoDao.Get(id);
+
+            if (veiculo == null)
+                return RedirectToAction("Index");
+
+            ViewBag.Veiculo = veiculo;
+
+            ViewBag.Motoristas = _motoristaDao.GetAll();
+            ViewBag.Funcionarios = _funcionarioDao.GetAll();
+
+            return View();
+        }
+        
+        [HttpPost]
+        [Route("autorizacao/novo")]
+        [Transaction]
+        public ActionResult Novo(AutorizacaoCirculacao autorizacao)
+        {
+            autorizacao.Numero = 1; // TODO Implementatar classe para gerar o número automaticamente 
+            autorizacao.Ano = autorizacao.Data.Year;
+
+            try
+            {
+                ValidationResult result = _validation.Validate(autorizacao);
+
+                if (result.IsValid)
+                {
+                    _autorizacaoCirculacaoDao.Save(autorizacao);
+
+                    return RedirectToAction("Index", "Autorizacao", new {id = autorizacao.Id});
+                }
+
+                ViewBag.Veiculo = _veiculoDao.Get(autorizacao.Veiculo.Id);
+
+                ViewBag.Motoristas = _motoristaDao.GetAll();
+                ViewBag.Funcionarios = _funcionarioDao.GetAll();
+
+                return View(autorizacao);
+            }
+            catch (Exception)
+            {
+                return View(autorizacao);
+            }
         }
 
     }
---
+++ Veiculos/Models/Validation/AutorizacaoCirculacaoValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentValidation;
+
+namespace Veiculos.Models.Validation
+{
+    public class AutorizacaoCirculacaoValidation : AbstractValidator<AutorizacaoCirculacao>
+    {
+
+        public AutorizacaoCirculacaoValidation()
+        {
+            RuleFor(a => a.Numero).GreaterThan(0);
+            RuleFor(a => a.Ano).GreaterThanOrEqualTo(DateTime.Now.Year - 1);
+            RuleFor(a => a.Ano).LessThanOrEqualTo(DateTime.Now.Year);
+
+            RuleFor(a => a.Inicio).LessThanOrEqualTo(a => a.Termino);
+            RuleFor(a => a.Finalidade).NotEmpty();
+            RuleFor(a => a.QuemAutorizou).NotNull();
+        }
+
+    }
+}
