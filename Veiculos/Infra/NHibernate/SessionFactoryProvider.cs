41f4dd7c9ab8deede00706b1ad222e0111e64fdf
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-11-07 23:55:16 -0400

    Separando a string de conexão em uma constante.

--- Veiculos/Infra/NHibernate/SessionFactoryProvider.cs
+++ Veiculos/Infra/NHibernate/SessionFactoryProvider.cs
@@ -15,16 +15,16 @@ namespace Veiculos.Infra.NHibernate
 
         public ISessionFactory BuildSessionFactory()
         {
+            const string conexao = @"Data Source=G1711MAX\sqlexpress;Password=chapado;User ID=sa;Initial Catalog=veiculos;Application Name=Veiculos;";
+
             return Fluently.Configure()
-                .Database(
-                    MsSqlConfiguration.MsSql2008.ConnectionString(
-                        @"Data Source=G1711MAX\sqlexpress;Password=chapado;User ID=sa;Initial Catalog=veiculos;Application Name=Veiculos;")
-                        .ShowSql())
+                .Database(MsSqlConfiguration.MsSql2008
+                                            .ConnectionString(conexao)
+                                            .ShowSql())
                 .Mappings(m => m.FluentMappings.AddFromAssemblyOf<VeiculoMap>())
                 .ExposeConfiguration(c => c.SetProperty("generate_statistics", "true"))
                 //.ExposeConfiguration(c => new SchemaExport(c).Create(false, true)) // TODO Remover depois de definir todos os models
                 .BuildSessionFactory();
         }
-
     }
 }
