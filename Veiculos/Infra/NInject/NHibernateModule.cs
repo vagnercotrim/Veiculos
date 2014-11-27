4b114f52acc076aa7341e56a380b12dac688a7c0
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2014-11-27 19:34:33 -0400

    Alteração do escopo de injeção de dependência para IStatistics.

--- Veiculos/Infra/NInject/NHibernateModule.cs
+++ Veiculos/Infra/NInject/NHibernateModule.cs
@@ -19,7 +19,7 @@ namespace Veiculos.Infra.NInject
             Kernel.BindFilter<TransactionFilter>(FilterScope.Action, 0).WhenActionMethodHas<TransactionAttribute>();
 
             Kernel.Bind<CriteriaPaginate>().To<CriteriaPaginate>().InRequestScope();
-            Kernel.Bind<IStatistics>().ToProvider<StatisticsProvider>().InSingletonScope();
+            Kernel.Bind<IStatistics>().ToProvider<StatisticsProvider>().InRequestScope();
         }
     }
 }
