8c1f21f7f35de85770ea14e988d1e675b08b83ce
Vagner Cotrim <vagner_cotrim@yahoo.com.br>
2015-03-18 22:25:46 -0400

    Instalando HangFire e suas dependências.

--- Veiculos/Startup.cs
+++ Veiculos/Startup.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.Owin;
+using Ninject;
+using Ninject.Web.Common;
 using Owin;
 
 [assembly: OwinStartup(typeof(Veiculos.Startup))]
@@ -10,7 +14,27 @@ namespace Veiculos
         public void Configuration(IAppBuilder app)
         {
             app.MapSignalR();
+            ConfigureHangFire(app);
         }
 
+        private void ConfigureHangFire(IAppBuilder app)
+        {
+            const string conexao = @"Data Source=G1711MAX\sqlexpress;Password=chapado;User ID=sa;Initial Catalog=veiculos;Application Name=Veiculos;";
+
+            app.UseHangfire(config =>
+            {
+                config.UseSqlServerStorage(conexao);
+                config.UseServer();
+                config.UseNinjectActivator(GetKernel());
+                config.UseDashboardPath("/tarefas");
+            });
+        }
+
+        private IKernel GetKernel()
+        {
+            Bootstrapper bootstrapper = new Bootstrapper();
+
+            return bootstrapper.Kernel;
+        }
     }
 }
